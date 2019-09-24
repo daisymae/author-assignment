5224e3d41bafd37eed4f38b047de1c95ed884442
daisymae <daisymae.orcadiafarm@gmail.com>
2019-09-24 11:57:22 -0500

    Add Authors
    Practice creating controller and thymeleaf template by adding authors page.

---
+++ src/main/java/guru/springframework/spring5webapp/controllers/AuthorController.java
@@ -0,0 +1,28 @@
+package guru.springframework.spring5webapp.controllers;
+
+import guru.springframework.spring5webapp.repositories.AuthorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AuthorController {
+
+  private AuthorRepository repository;
+
+  public AuthorController(AuthorRepository repository) {
+    this.repository = repository;
+  }
+
+  /**
+   * get the authors for viewing
+   * @param model the model
+   * @return the view name
+   */
+  @RequestMapping("/authors")
+  public String getAuthors(Model model) {
+    model.addAttribute("authors", repository.findAll());
+    return "authors";
+  }
+}
